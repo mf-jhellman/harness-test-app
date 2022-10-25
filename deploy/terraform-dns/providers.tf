35d1761c8c08e96df56a2ebd729120c0e317137c
Greg Ganz <greg.ganz@markforged.com>
2022-10-25 14:41:41 -0400

    [PATCH] Ingress chart and terraform

---
+++ deploy/terraform-dns/providers.tf
@@ -0,0 +1,26 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Terraform state info
+terraform {
+  backend "s3" {
+    bucket  = "tf-state-mf-eiger-dev-default"
+    key     = "state/octopus-dummy-example-dns"
+    region  = "us-east-1"
+    encrypt = true
+  }
+}
+
+
+data "aws_caller_identity" "current" {}
+
+# Resource tagging
+locals {
+  account_id       = data.aws_caller_identity.current.account_id
+  tags = {
+    environment = "devops"
+    terraform   = "managed"
+  }
+}
